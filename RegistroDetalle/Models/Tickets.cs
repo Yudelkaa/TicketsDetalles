4aa5f008beec70c5ca5269a5fb7c12e17a22a2b5
Yudelka <yudelkaguillen07@gmail.com>
2024-02-25 08:16:44 -0400

    Modelos y create

---
+++ RegistroDetalle/Models/Tickets.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDetalle.Models
+{
+	public class Tickets
+	{
+		[Key]
+		public int TicketId { get; set; }
+
+		public int ClienteId { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public DateTime Fecha { get; set; } = DateTime.Now;
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public string? Asunto { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public string? Descripcion { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		[StringLength(30, ErrorMessage = "No puede exceder los 30 Caracteres")]
+		public string? SolicitadoPor { get; set; }
+
+		[ForeignKey("TicketId")]
+		public ICollection<TicketsDetalle> TicketsDetalle { get; set; } = new List<TicketsDetalle>();
+
+	}
+}
---
+++ RegistroDetalle/Models/TicketsDetalle.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDetalle.Models
+{
+	public class TicketsDetalle
+	{
+		[Key]
+		public int DetalleId { get; set; }
+		public int TicketId { get; set; }
+
+		[Required(ErrorMessage = "Campo obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo no acepta números")]
+		public string? Emisor { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public string? Mensaje { get; set; }
+	}
+}
