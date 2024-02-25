58f4e2f48f7a1849eef1eea571fc6db5faf1bc50
Yudelka <yudelkaguillen07@gmail.com>
2024-02-25 10:04:00 -0400

    Contexto

---
+++ RegistroDetalle/Context/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDetalle.Models;
+using System.Collections.Generic;
+
+namespace RegistroDetalle.Api.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Tickets> Tickets { get; set; }
+
+    }
+}
+
